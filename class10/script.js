3512f4626642c388a28ed018c8361959fd722c4a
dhamoleom9-spec <dhamoleom9@gmail.com>
2025-12-01 13:32:29 +0530

    DOM 11-2

--- class10/script.js
+++ class10/script.js
@@ -7,3 +7,51 @@ addEventListener('mousemove', function (e) {
     document.body.style.setProperty("--x", e.clientX + "px")
     document.body.style.setProperty("--y", e.clientY + "px")
 })
+
+let h2 = document.querySelector("h1")
+let charactors = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+let text = h2.innerHTML
+
+// let iteration = 0
+
+// h1.addEventListener('mouseenter', () => {
+
+//     setInterval(() => {
+//         const str = text.split("").map((char, index) => {
+
+//             if (index < iteration) {
+//                 return char;
+//             }
+
+//             return charactors.split("")[Math.floor(Math.random() * 52)]
+
+//         })
+//         h1.innerHTML = str
+
+//         iteration += 0.2
+//     }, 30)
+
+// })
+
+
+let i = 0
+
+function play() {
+
+    let str = text.split("").map((char, index) => {
+        if(index < i){
+            return char;
+        }
+        return charactors.split("")[Math.floor(Math.random() * 52)]
+    }).join("")
+
+    h2.innerHTML = str
+
+    i += 0.2
+}
+
+h2.addEventListener('mouseenter', ()=>{
+
+setInterval(play, 20)
+
+})
